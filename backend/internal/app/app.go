4c3cf08a1efa5d9d7a714cbb6a65db33b0753cb9
Nixon Kosgei <nkosgey6@gmail.com>
2023-12-24 05:48:57 +0300

    initialize app with a DB connection

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -6,12 +6,15 @@
 package app
 
 import (
+	"database/sql"
 	"github.com/nixoncode/out-of-the-abyss/cmd"
+	"github.com/nixoncode/out-of-the-abyss/internal/db/sqlc"
 	"github.com/spf13/cobra"
 )
 
 type App struct {
 	RootCmd *cobra.Command
+	DB      *sqlc.Queries
 }
 
 func (app *App) Start() error {
@@ -24,11 +27,12 @@ func (app *App) Execute() error {
 	return app.RootCmd.Execute()
 }
 
-func New() *App {
+func New(db *sql.DB) *App {
 	return &App{
 		RootCmd: &cobra.Command{
 			Use:   "out-of-the-abyss",
 			Short: "Out of the abyss CLI",
 		},
+		DB: sqlc.New(db),
 	}
 }
--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,28 @@ Copyright © 2023 Nixon Kosgei <nkosgey6@gmail.com>
 package main
 
 import (
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/nixoncode/out-of-the-abyss/internal/app"
 )
 
+var DB *sql.DB
+
+func init() {
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/out_of_the_abyss")
+	if err != nil {
+		panic("cannot connect to the database")
+	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	DB = db
+}
 func main() {
-	application := app.New()
+	application := app.New(DB)
 
 	err := application.Start()
 	if err != nil {
